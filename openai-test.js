60535ab17c712f1c1336ba24fa3ea1c0751971ff
jonathan <joncanales2@gmail.com>
2023-12-21 20:00:59 -0500

    updated gpt model as 3.5 made it difficult to have efficient code.

--- openai-test.js
+++ openai-test.js
@@ -10,7 +10,7 @@ const {
   specificFiles,
   appendToFile,
 } = require('./file-functions.js');
-const { askQuestion } = require('./chat-functions');
+const { askQuestion, parseCommandAndWriteToFile } = require('./chat-functions');
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
@@ -53,13 +53,20 @@ async function main() {
 
     const completion = await openai.chat.completions.create({
       messages: messages,
-      model: 'gpt-3.5-turbo',
+      model: 'gpt-4',
     });
 
     const botMessage = completion.choices[0].message.content;
     console.log('ChatGPT:'.green, botMessage.green);
 
     messages.push({ role: 'assistant', content: botMessage });
+
+      // Use the imported function to handle file writing
+      const fileWriteResult = parseCommandAndWriteToFile(userMessage, botMessage, __dirname);
+      if (fileWriteResult) {
+        console.log(fileWriteResult.bgYellow);
+      }
+  
   }
 }
 
