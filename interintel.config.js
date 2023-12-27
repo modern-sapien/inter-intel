3b90b5d9f65312949ae71edf1791dee30907eb72
jonathan <joncanales2@gmail.com>
2023-12-27 14:29:11 -0500

    config updated, setup function also updated to provide better error handling.

--- interintel.config.js
+++ interintel.config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const config = {
   apiKey: `${process.env.OPENAI_API_KEY}`,
-  aiVersion: `gpt-4`,
+  aiVersion: `gpt-3.5-turbo`,
   filePaths: ['./inter-intel/training/reference.txt', './readme.md'],
 };
 
--- setup.js
+++ setup.js
@@ -4,9 +4,25 @@ const path = require('path');
 const configPath = path.join(process.cwd(), 'interintel.config.js');
 const templatePath = path.join(__dirname, 'interintel.config.template.js');
 
-if (!fs.existsSync(configPath)) {
-    fs.copyFileSync(templatePath, configPath);
-    console.log('Interintel config created. Please update it with your settings.'.yellow);
-} else {
-    console.log('Interintel config file already exists.'.yellow);
+console.log("Current working directory:", process.cwd());
+
+console.log(`Checking for config file at: ${configPath}`);
+
+console.log(fs.existsSync(configPath), "config path exists?")
+
+console.log(`Template file path: ${templatePath}`);
+
+console.log(fs.existsSync(templatePath), "template config path exists?")
+
+
+try {
+    if (!fs.existsSync(configPath)) {
+        console.log('Config file does not exist, creating...');
+        fs.copyFileSync(templatePath, configPath);
+        console.log('Interintel config created. Please update it with your settings.'.yellow);
+    } else {
+        console.log('Interintel config file already exists.');
+    }
+} catch (error) {
+    console.error('Error occurred during setup:', error);
 }
