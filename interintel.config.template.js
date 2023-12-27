b3ed899159e16e62b684df878d49d9bba0162fa4
jonathan <joncanales2@gmail.com>
2023-12-27 11:40:58 -0500

    should create config when installing interintel moving forward.

---
+++ interintel.config.template.js
@@ -0,0 +1,9 @@
+
+
+const config = {
+  apiKey: `${process.env.OPENAI_API_KEY}`,
+  aiVersion: `ONLY_USE_OPENAI_MODEL`,
+  filePaths: ['THIS', 'IS', 'ARRAY', 'OF', 'STRINGS'],
+};
+
+module.exports = config;
---
+++ npmsetup.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+
+const configPath = path.join(__dirname, 'interintel.config.js');
+const templatePath = path.join(__dirname, 'interintel.config.template.js');
+
+if (!fs.existsSync(configPath)) {
+    fs.copyFileSync(templatePath, configPath);
+    console.log('Interintel config created. Please update it with your settings.'.yellow);
+} else {
+    console.log('Interintel config file already exists.'.yellow);
+}
