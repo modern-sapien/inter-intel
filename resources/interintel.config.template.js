c1d6f4232142c0fc527236d95ec2650e2048fbc0
jonathan <joncanales2@gmail.com>
2024-01-03 20:56:56 -0500

    updated config template.

--- resources/interintel.config.template.js
+++ resources/interintel.config.template.js
@@ -1,9 +1,12 @@
 require('dotenv').config();
 
 const config = {
+  // this is service dependent
   apiKey: `${process.env.OPENAI_API_KEY}`,
-  // only open ai models for now
-  aiVersion: `ONLY_USE_OPENAI_MODEL`,
+  // openai || ollama
+  aiService: 'ONE_OF_THE_ABOVE',
+  // only open ai or ollama models for now
+  aiVersion: `ONLY_USE_OPENAI_OR_OLLAMA_MODELS`,
   // These filepaths are relative to where your config is created
   filePaths: ['interintel/interintelReadMe.md'],
 };
